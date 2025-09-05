ab727f4bbc9571edee88d757709deaedb5219154
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-09-05 22:05:47 +0530

    jfs41

---
+++ Pattern_01.java
@@ -0,0 +1,24 @@
+package com.codegnan.controlstatements;
+
+public class Pattern_01 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				//System.out.print("* ");
+				//System.out.print(j + " ");
+				//System.out.print(i + " ");
+				//System.out.print((6-j)+ " ");
+				//System.out.print((6-i)+ " ");
+				//System.out.print((char)(96+j)+ " ");
+				//System.out.print((char)(96+i)+ " ");
+				//System.out.print((char)(102-j)+ " ");
+				System.out.print((char)(102-i)+ " ");
+			}
+			System.out.println();
+			}
+		}
+
+	}
+
+
